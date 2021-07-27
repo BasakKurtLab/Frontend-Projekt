d5397b9a39604586cd18afe1b38dfe144ad0d899
BasakKurtLab <79793959+BasakKurtLab@users.noreply.github.com>
2021-07-27 13:07:26 +0200

    up to scroll added

--- script.js
+++ script.js
@@ -204,4 +204,24 @@ function menuShow()
             nav.style.height = "250px";
     
             menuOn = !menuOn;
-    }
+}
+
+
+const mybutton = document.getElementById("scrollBtn");
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
+}
